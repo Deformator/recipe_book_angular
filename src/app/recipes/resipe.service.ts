1fb13ea2dc7b5062d2be66108f87ea671e30d532
Andrii Damm <andrii@unionms-iMac.local>
2018-12-03 15:34:05 -0500

    Change event emmiter with observable

--- src/app/recipes/resipe.service.ts
+++ src/app/recipes/resipe.service.ts
@@ -1,12 +1,10 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
 @Injectable()
 export class RecipeService {
-
-    recipeSelected = new EventEmitter<Recipe>()
 
    private recipes: Recipe[] = [
         new Recipe('Shnizzel', 'I did not try a shnizzel', 'http://blumfarr.de/wp-content/uploads/2015/03/gg.de-15.jpg', [new Ingredient('meat', 1), new Ingredient('Fry potato', 5)]),
